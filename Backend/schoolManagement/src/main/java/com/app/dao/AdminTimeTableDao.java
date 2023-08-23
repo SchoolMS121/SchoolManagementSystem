a25e2f248e6ba7c137cb760f0441826a1ff823e6
Hitesh Sonawane <hiteshss1718@gmail.com>
2023-08-23 12:54:07 +0530

    Backend Deleted

--- Backend/schoolManagement/src/main/java/com/app/dao/AdminTimeTableDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Timetable;
-
-public interface AdminTimeTableDao extends JpaRepository<Timetable, Long> {
-
-}
--- Backend/schoolManagement/src/main/java/com/app/dao/ClassroomDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Classroom;
-
-public interface ClassroomDao extends JpaRepository<Classroom, Long>{
-
-}
